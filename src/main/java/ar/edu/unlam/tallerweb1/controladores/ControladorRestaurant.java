0c2d6ae95f4b0fa3b4df799260e4957d3814babf
roxanacano26 <roxana.cano@mercadolibre.com>
2020-05-18 02:27:04 -0300

    agregue test de persistencia para obtener menues de un determinado restaurante

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorRestaurant.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorRestaurant.java
@@ -1,16 +1,16 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
 import ar.edu.unlam.tallerweb1.modelo.Menu;
-import ar.edu.unlam.tallerweb1.modelo.Restaurant;
-import ar.edu.unlam.tallerweb1.modelo.Usuario;
-import ar.edu.unlam.tallerweb1.servicios.ServicioLogin;
 import ar.edu.unlam.tallerweb1.servicios.ServicioRestaurant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+
 
 @Controller
 public class ControladorRestaurant {
@@ -22,7 +22,7 @@ public class ControladorRestaurant {
         this.servicioRestaurant = servicioRestaurant;
     }
 
-    @RequestMapping("/restaurant/menu/{id}")
+    @RequestMapping(path = "/restaurant/menu/{id}",method = RequestMethod.POST)
     public ModelAndView mostrarMenu(@PathVariable Long id) {
         ModelMap modelo = new ModelMap();
         Menu menuBuscado = servicioRestaurant.consultarMenu(id);
